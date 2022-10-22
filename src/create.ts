7bf273584f667beb9aecc6f9e9ddc7bd4648b16f
Amine Ben hammou <webneat@gmail.com>
2022-10-22 14:00:07 +0200

    Release 1.0.0-beta.1

--- src/create.ts
+++ src/create.ts
@@ -1,11 +1,11 @@
-import {createStore} from 'redux'
+import {legacy_createStore} from 'redux'
 import {flattenHandlers} from './flattenHandlers'
 import {makeActions} from './makeActions'
 import {makeReducer} from './makeReducer'
 import {Handlers} from './types'
 
 export function create<State, Fns extends Handlers<State>>(initialState: State, fns: Fns) {
-  const store = createStore(
+  const store = legacy_createStore(
     makeReducer(initialState, flattenHandlers(fns)),
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
   )
