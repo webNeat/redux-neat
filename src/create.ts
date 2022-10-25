92c5582a139b3b45b3450d504e6004c973b57505
Amine Ben hammou <webneat@gmail.com>
2022-10-25 22:23:41 +0200

    Release 1.0.0-beta.3

--- src/create.ts
+++ src/create.ts
@@ -7,7 +7,7 @@ import {Handlers, Actions} from './types'
 export function create<State, Fns extends Handlers<State>>(initialState: State, fns: Fns) {
   const store = legacy_createStore(
     makeReducer(initialState, flattenHandlers(fns)),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    window && (window as any)?.__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
   )
   const actions: Actions<Fns> = makeActions(store, fns)
   return {store, actions}
