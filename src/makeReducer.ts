f20d00cce6e03d823b613f30e141737d426618ef
Amine Ben hammou <webneat@gmail.com>
2022-11-02 23:51:21 +0100

    Release 1.0.0-beta.5

--- src/makeReducer.ts
+++ src/makeReducer.ts
@@ -1,11 +1,14 @@
 import {Reducer} from 'redux'
 import {Handlers} from './types'
+import {deepClone} from './utils'
 
 export function makeReducer<State, Fns extends Handlers<State, 1>>(initialState: State, handlers: Fns): Reducer<State> {
   return (state = initialState, action) => {
     for (const name in handlers) {
       if (action.type == name) {
-        return handlers[name]({...state}, ...action.args)
+        state = deepClone(state)
+        handlers[name](state, ...action.args)
+        return state
       }
     }
     return state
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,7 @@ export function isFunction<T>(x: T): T extends Function ? true : false
 export function isFunction(x: any) {
   return !!x && {}.toString.call(x) === '[object Function]'
 }
+
+export function deepClone<T>(state: T): T {
+  return JSON.parse(JSON.stringify(state))
+}
